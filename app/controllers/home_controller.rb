708e1a967f131208937da1a0de7cc1130bdb43d3
lomochka <yfgurda@gmail.com>
2019-01-15 19:26:04 +0200

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class HomeController < ApplicationController
+  # GET users/
+  def index; end
+
+  # GET users/:id
+  def show; end
+
+  # PATCH/PUT users/:id
+  def update; end
+
+  # DELETE users/:id
+  def destroy; end
+end
