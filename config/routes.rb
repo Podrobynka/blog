bcfdbe8b4b9580370225bb9353abf35ae37c68f4
lomochka <yfgurda@gmail.com>
2019-01-17 00:36:57 +0200

    display validation errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # get '/' => 'home#index'
+  get '/' => 'home#index'
   get 'home/index'
+
+  get 'contacts' => 'contacts#new'
   resources :articles
-  resource :contacts, only: %i[new create]
+  resource :contacts, only: %i[create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
