4e1e5ec386a7027cd921970183c03a23c324dafe
lomochka <yfgurda@gmail.com>
2019-01-16 23:16:38 +0200

    add new contact form, update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # get '/' => 'home#index'
   get 'home/index'
   resources :articles
-  resource :contacts, only: [:new, :create]
+  resource :contacts, only: %i[new create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
