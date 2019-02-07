29b3d6a0476eb3fe98014f8ed0935c9a32e2d420
lomochka <yfgurda@gmail.com>
2019-02-07 22:56:23 +0200

    display author of comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,12 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!, :only => [:create]
 
   def create
-    @article = Article.find(params[:article_id])
-    @article.comments.create(comment_params)
-    redirect_to @article
+    article = Article.find_by(id: params[:article_id])
+    current_user
+      .comments
+      .create(comment_params.merge(article_id: params[:article_id]))
+
+    redirect_to article
   end
 
   private
