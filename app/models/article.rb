06d03af77b4dc2237f9e9bd0cf9a1a0fabbbfd97
lomochka <yfgurda@gmail.com>
2019-02-13 00:50:28 +0200

    add validation of length for title and text in article

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,8 +7,14 @@ class Article < ApplicationRecord
 
   validates :title, presence: true
   validates :text, presence: true
+  validates :title, length: { maximum: 140 }
+  validates :text, length: { maximum: 4000 }
 
   def subject
     title
+  end
+
+  def last_comment
+    comments.last
   end
 end
